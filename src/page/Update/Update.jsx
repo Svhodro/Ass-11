c40a048805018539f85dab1912d0a18dab5e516c
Svhodro <rudrolipi@gmail.com>
2024-05-13 14:30:28 +0600

    entry mahakal

--- src/page/Update/Update.jsx
+++ src/page/Update/Update.jsx
@@ -8,7 +8,7 @@ import axios from 'axios';
 function Update() {
   const { update, setupdate } = useContext(UserContext)
   const [categore, setcategore] = useState()
- 
+
   const handlebus = () => {
     setcategore('business')
   }
@@ -22,45 +22,53 @@ function Update() {
     setcategore('religion')
   }
   const notify = () => toast("You Have Sucssesfully Update The Book");
-  const handleupdate = async(e) => {
+  const handleupdate = async (e) => {
     e.preventDefault();
-    const id =update
+    const id = update
     const img = e.target.url.value;
     const name = e.target.bookname.value;
     const author = e.target.authorname.value;
     const category = categore
     const rateing = e.target.rate.value
-    const updatedata={
-      img:img,
-       name:name,
-       author:author,
-       category:category,
-       rateing:rateing
+    const updatedata = {
+      Bookid: id,
+      img: img,
+      name: name,
+      author: author,
+      category: category,
+      rateing: rateing
     }
     // console.log(img,name,author,category,rating,id)
-  
 
-   try {
-    await axios.put(`https://ass-11-server-ten.vercel.app/Update/${id}`,updatedata)
-    .then(res=>{
-         notify()
-    })
-    // Handle success (e.g., show a success message)
+    // problem ta akhane
+    try {
+      const putRequest = {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+          mode: 'no-cors',
+        },
+        body: JSON.stringify(updatedata),
+      };
+      await fetch('https://ass-11-server-ten.vercel.app/Update',putRequest)
 
-  } catch (error) {
-    // Handle error (e.g., show an error message)
 
-    console.log(error.message)
+      // Handle success (e.g., show a success message)
+
+    } catch (error) {
+      // Handle error (e.g., show an error message)
+
+      console.log(error.message)
+    }
   }
-  }
- 
-  
+
+
   return (
     <div className='w-full flex justify-center items-center'>
       <div className='w-full sm:w-2/3 md:w-1/ lg:1/3 '>
         <form className="card-body" onSubmit={(e) => {
-            handleupdate(e);
-          }}>
+          handleupdate(e);
+        }}>
 
           <div className="form-control" >
             <label className="label">
@@ -83,10 +91,10 @@ function Update() {
           <div className="dropdown dropdown-bottom">
             <div tabIndex={0} role="button" className="btn m-1">Category</div>
             <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
-              <li onClick={handleself }><a>Self-Helf</a></li>
-              <li onClick={ handlerele}><a>Religion</a></li>
-              <li onClick={ handlebus}><a>Business</a></li>
-              <li onClick={handlemis }><a>MyStery</a></li>
+              <li onClick={handleself}><a>Self-Helf</a></li>
+              <li onClick={handlerele}><a>Religion</a></li>
+              <li onClick={handlebus}><a>Business</a></li>
+              <li onClick={handlemis}><a>MyStery</a></li>
             </ul>
           </div>
           <div className="form-control">
